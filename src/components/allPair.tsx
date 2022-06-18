4b812e12bde32b06a5f7fe22b25a966db501ecd9
Julia Medvedeva <60058448+julmedvedeva@users.noreply.github.com>
2022-06-18 10:26:48 +0300

    Feat/2 (#3)
    * добавила скелетон и обработку успеха покупки

--- src/components/allPair.tsx
+++ src/components/allPair.tsx
@@ -2,38 +2,42 @@ import React, { useCallback, useEffect } from 'react';
 import { useRootStore } from '../RootStateContext';
 import { observer } from 'mobx-react-lite';
 import { Accordion } from 'react-bootstrap';
-
+import Skeleton from 'react-loading-skeleton';
 
 const PairList = () => {
   const { pairStore } = useRootStore();
-  const fetchData = useCallback(async () => {
-    await pairStore.loadPairs();
+  const fetchData = useCallback(() => {
+    pairStore.loadPairs();
   }, []);
   useEffect(() => {
     fetchData();
   }, []);
   return (
     <>
-      <Accordion>
-        {pairStore.pairs?.map((pair: any, index: number) => {
-          return (
-            <Accordion.Item eventKey={index.toString()}>
-              <Accordion.Header>
-                {`${pair.base} => ${pair.counter}`}
-              </Accordion.Header>
-              <Accordion.Body>
-                <div>{`base: ${pair.base}`}</div>
-                <div>{`baseAddress: ${pair.baseAddress}`}</div>
-                <div>{`counter: ${pair.counter}`}</div>
-                <div>{`counterAddress: ${pair.counterAddress}`}</div>
-                <div>{`fee: ${pair.fee}`}</div>
-                <div>{`lpAddress: ${pair.lpAddress}`}</div>
-                <div>{`poolAddress: ${pair.poolAddress}`}</div>
-              </Accordion.Body>
-            </Accordion.Item>
-          );
-        })}
-      </Accordion>
+      {pairStore.isLoading ? (
+        <Skeleton count={5} height={30} />
+      ) : (
+        <Accordion>
+          {pairStore.pairs?.map((pair: any, index: number) => {
+            return (
+              <Accordion.Item eventKey={index.toString()}>
+                <Accordion.Header>
+                  {`${pair.base} => ${pair.counter}`}
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div>{`base: ${pair.base}`}</div>
+                  <div>{`baseAddress: ${pair.baseAddress}`}</div>
+                  <div>{`counter: ${pair.counter}`}</div>
+                  <div>{`counterAddress: ${pair.counterAddress}`}</div>
+                  <div>{`fee: ${pair.fee}`}</div>
+                  <div>{`lpAddress: ${pair.lpAddress}`}</div>
+                  <div>{`poolAddress: ${pair.poolAddress}`}</div>
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
+      )}
     </>
   );
 };
