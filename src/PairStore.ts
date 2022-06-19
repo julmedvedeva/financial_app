7f433dac1b0d40a8d5734b15a92631aa8be34bd5
Julia Medvedeva <medvedevajulev@gmail.com>
2022-06-19 12:23:36 +0300

    clean  up

---
+++ src/PairStore.ts
@@ -0,0 +1,36 @@
+import { observable, action, makeAutoObservable } from 'mobx';
+import { buyPair, getPair } from './service/service';
+
+export class PairStore {
+  constructor() {
+    makeAutoObservable(this);
+  }
+  @observable pairs: any[] | undefined = [];
+  @observable state: string | undefined = '';
+  @observable isLoading: boolean = true;
+
+  @action
+  loadPairs = () => {
+    setTimeout(() => {
+      getPair().then((pairs) => {
+        const filterData = (arr: any, query: string, ent: string) => {
+          return arr.filter((el: any) => el[ent] === query);
+        };
+        const finishAr = filterData(pairs, 'USDT', 'counter');
+        this.pairs = [...finishAr];
+      });
+      this.isLoading = false;
+    }, 5000);
+  };
+
+  @action buyPairs = () => {
+    this.state = buyPair();
+    switch (this.state) {
+      case 'fail':
+        alert('EROOR! TRY LATER');
+        break;
+      case 'success':
+        break;
+    }
+  };
+}
